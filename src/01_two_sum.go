66454f35cf62216cf3e171e087946e5dd2d1a5d8
johncurcio <joao8177@gmail.com>
2019-08-17 00:14:59 -0300

    change source code of 01 to golang

---
+++ src/01_two_sum.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, num := range nums {
+        index, ok := m[target-num]
+        if ok {
+            return []int{index, i}
+        } else {
+            m[num] = i
+        }
+    }
+    return []int{}
+}
