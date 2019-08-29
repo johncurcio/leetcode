f2411733bd5393cc9e8d1fc537427ee5a6de1b23
Joao Curcio <john@curcio.dev>
2019-08-29 10:51:58 -0300

    add go lang version of problem 3: length of the longest substring with no repeating characters

---
+++ src/03_length_longest_substring_no_repeating_chars.go
@@ -0,0 +1,19 @@
+func lengthOfLongestSubstring(s string) int {
+    start, end := 0, 0
+    maxLen := 0
+    set := make(map[string]bool)
+    for start < len(s) && end < len(s) {
+        str := string(s[end])
+        if set[str] {
+            delete(set, string(s[start]))
+            start = start + 1
+        } else {
+            set[str] = true
+            end = end + 1
+        }
+        if len(set) > maxLen {
+            maxLen = len(set)   
+        }
+    }
+    return maxLen
+}
