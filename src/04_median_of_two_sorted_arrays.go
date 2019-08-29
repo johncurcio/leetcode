86f8f54937059f0458010c9673f6bba5844be71f
Joao Curcio <john@curcio.dev>
2019-08-29 17:56:51 -0300

    add go lang version of problem 4: median of two sorted arrays

---
+++ src/04_median_of_two_sorted_arrays.go
@@ -0,0 +1,41 @@
+/*
+O(n+m) solution. Should be O(log(n+m)). Needs improvement
+*/
+
+func getMedian(nums []int) float64 {
+    size := len(nums)
+    if size % 2 == 0 {
+        return float64(nums[size/2-1] + nums[size/2]) / 2
+    }
+    return float64(nums[size/2])
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    ptr1, ptr2, index := 0, 0, 0 
+    size := len(nums1) + len(nums2)
+    merged := make([]int, size)
+    for ptr1 < len(nums1) && ptr2 < len(nums2) {
+        if nums1[ptr1] < nums2[ptr2] {
+            merged[index] = nums1[ptr1]
+            ptr1 = ptr1 + 1
+        } else {
+            merged[index] = nums2[ptr2]
+            ptr2 = ptr2 + 1
+        }
+        index = index + 1
+    }
+    
+    for ptr1 < len(nums1) {
+        merged[index] = nums1[ptr1]
+        ptr1 = ptr1 + 1
+        index = index + 1
+    }
+    
+    for ptr2 < len(nums2) {
+        merged[index] = nums2[ptr2]
+        ptr2 = ptr2 + 1
+        index = index + 1
+    }
+
+    return getMedian(merged)
+}
